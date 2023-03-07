ec3b3db5e99e291cbbcc7487bbb22b6e131e0eee
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-03-07 17:05:04 +0530

    GetElementsbyClassname

---
+++ dom1.js
@@ -0,0 +1,7 @@
+
+var change=document.getElementsByClassName('list-group-item');
+console.log(change)
+change[2].style.backgroundColor="green"
+for(var i=0;i<change.length;i++){
+    change[i].style.fontWeight="bold"
+}
