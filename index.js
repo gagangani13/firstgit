65fa99353db81670a99113c3200a3d480a8b20be
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-03-08 23:32:37 +0530

    Lets store the users in the right format

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ function store(e){
     var email=document.getElementById('mail').value;
     var time=document.getElementById('call').value; 
     var time=document.getElementById('call').value;   
-    var arr=new Array
-    arr.push(names,email,time)
-    localStorage.setItem(mobile,arr)
+   
+    //arr.push(names,email,time)
+    //localStorage.setItem(mobile,arr)
+    var arr=({
+        'name': names,
+        'email': email,
+        'time': time
+    })
+    var arr1=({'mobile':mobile})
+    let sarr=JSON.stringify(arr)
+    let sarr1=JSON.stringify(arr1)
+    localStorage.setItem(sarr,sarr1)
 }
 
     
