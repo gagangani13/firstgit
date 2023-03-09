54dde025436394e56524b448957e618ccb20ed1e
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-03-09 11:48:01 +0530

    Delete data from Local Storage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var parentlist=document.querySelector('.addhere')
 var btn=document.getElementById('btn');
 btn.addEventListener('click',store);
 function store(e){
@@ -18,18 +18,26 @@ function store(e){
         'email': email,
         'time': time
     })
-    var arr1=({'mobile':mobile})
+    var arr1=mobile
     let sarr=JSON.stringify(arr)
-    let sarr1=JSON.stringify(arr1)
-    localStorage.setItem(sarr1,sarr)
+    //let sarr1=JSON.stringify(arr1)
+    localStorage.setItem(arr1,sarr)
 
     var li=document.createElement("li");
+    li.className=mobile;
     var addhere=document.querySelector('.addhere');
     var details=document.createTextNode(names+"->"+email+"->"+mobile+"->"+time)
     li.appendChild(details)
     addhere.appendChild(li)
-    
+    var delbut=document.createElement("button");
+    delbut.className='btn btn-danger'
+    delbut.appendChild(document.createTextNode("DELETE"))
+    li.appendChild(delbut)
+    console.log(li.mobile)
+    delbut.addEventListener('click',deleted);
+    function deleted(e){
+        var locate=e.target.parentElement;
+        localStorage.removeItem(locate.className)
+        parentlist.removeChild(locate);    
+    }  
 }
-
-
-    
